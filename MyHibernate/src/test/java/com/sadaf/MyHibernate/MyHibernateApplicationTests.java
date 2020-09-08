568ed3b17283a1f58ade00837ea3f4ac7b2e55f5
SadafHaroon2020 <sadafssuet05@gmail.com>
2020-09-08 14:06:53 -0500

    working

---
+++ MyHibernate/src/test/java/com/sadaf/MyHibernate/MyHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sadaf.MyHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
